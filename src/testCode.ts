f388d4df34cd0952efc524621ec5c8dfd5b3f1fa
BearSouL <mosakung@live.com>
2019-11-17 18:11:29 +0700

    Update app & test

--- src/testCode.ts
+++ src/testCode.ts
@@ -6,11 +6,15 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import config from 'config'
 
+//Require Module
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+
 //Set Class
 const app = express();
 
 //PROCESS_ENV
-//console.log(`NODE_ENV: ${process.env.NODE_ENV}` + ` || app: ${app.get('env')}`);
+console.log(`NODE_ENV: ${process.env.NODE_ENV}` + ` || app: ${app.get('env')}`);
 
 //APP USE
 app.use(express.json());
@@ -28,20 +32,19 @@ if (app.get('env') === 'DEV') {
 //Test Code
 
 import database from '../server/database';
-import routing from './routing';
+import Routing from './routing';
 
 const db = new database();
+const routing = new Routing();
 
 async function name() {
     const result = await db.query([],[]);
     console.log(result[0].ReviewBookID);
 }
 
-app.get('/hi', (req, res) => {
-    res.send('hello');
-})
-
 app.use('/shareed',routing);
+
+
 
 //
 
