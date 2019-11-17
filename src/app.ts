be39068fb8ab854952f193cfb7924dbb2821eee7
BearSouL <mosakung@live.com>
2019-11-17 18:23:12 +0700

    Update app

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
-//Import Package npm
 import express from 'express';
-import bodyParser from 'body-parser'
-import Joi from 'joi';
 import responseTime from 'response-time';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import config from 'config'
 
 //Set Class
 const app = express();
 
-app.use(express.json);
-app.use(bodyParser.json());
-
 //APP USE
+app.use(express.json());
+
 app.use(helmet());
 
 if (app.get('env') === 'DEV') {
@@ -24,27 +19,14 @@ if (app.get('env') === 'DEV') {
     app.use(morgan('tiny'));
 }
 
-//Router
-import routing from './routing'
+import routing from './routing';
 
-app.use('/shareed',routing);
-
-app.get('/hi', (req, res) => {
-    res.send('hello');
-})
-
-//Server
+app.use('/shareed', routing);
 
 app.listen(3000, () => {
-    console.log('Start server at port 3000.');
-});
-
-/*const server = app.listen(3000, () => {
     console.log(
         "  App is running at localhost:3000 in %s mode",
         app.get("env")
     );
-    console.log("Press CTRL-C to Terminate server\n");
-})
-
-export default {app, server};*/
+    console.log("  Press CTRL-C to stop\n");
+});
