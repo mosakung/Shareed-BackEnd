4b758e26500de30a85ead97081bf9e10224c1b62
akarapon.ty <akarapon.ty@hotmail.com>
2019-11-17 17:02:02 +0700

    edit viewServices

--- src/feature/View/viewServices.ts
+++ src/feature/View/viewServices.ts
@@ -1,22 +1,54 @@
 import Repo from './viewRepository';
 export default class viewServices {
+
     private repo: Repo;
     constructor(repo: Repo) {
         this.repo = repo;
     }
-    getShareNote = async (postID: string) => { return await this.repo.viewShareNote(postID); }
-    getShareNoteAll = async () => { return await this.repo.viewShareNoteAll(); }
-    getShareEvent = async (postID: string) => { return await this.repo.viewShareEvent(postID); }
-    getShareEventAll = async () => { return await this.repo.viewShareEventAll(); }
-    getReviewSubject = async (postID: string) => { return await this.repo.viewReviewSubject(postID); }
-    getReviewSubjectAll = async () => { return await this.repo.viewReviewSubjectAll(); }
-    getReviewTutor = async (postID: string) => { return await this.repo.viewReviewTutor(postID); }
-    getReviewTutorAll = async () => { return await this.repo.viewReviewTutorAll(); }
-    getReviewBook = async (postID: string) => { return await this.repo.viewShareNote(postID); }
-    getReviewBookAll = async () => { return await this.repo.viewReviewBookAll(); }
+    
+    getShareNote = async (postID: string) => {
+        try { return await this.repo.viewShareNote(postID); }
+        catch (err) { console.error('Error Services', err.message); }
+    }
+    getShareNoteAll = async () => {
+        try { return await this.repo.viewShareNoteAll(); }
+        catch (err) { console.error('Error Services', err.message); }
+    }
+    getShareEvent = async (postID: string) => {
+        try { return await this.repo.viewShareEvent(postID); }
+        catch (err) { console.error('Error Services', err.message); }
+    }
+    getShareEventAll = async () => {
+        try { return await this.repo.viewShareEventAll(); }
+        catch (err) { console.error('Error Services', err.message); }
+    }
+    getReviewSubject = async (postID: string) => {
+        try { return await this.repo.viewReviewSubject(postID); }
+        catch (err) { console.error('Error Services', err.message); }
+    }
+    getReviewSubjectAll = async () => {
+        try { return await this.repo.viewReviewSubjectAll(); }
+        catch (err) { console.error('Error Services', err.message); }
+    }
+    getReviewTutor = async (postID: string) => {
+        try { return await this.repo.viewReviewTutor(postID); }
+        catch (err) { console.error('Error Services', err.message); }
+    }
+    getReviewTutorAll = async () => {
+        try { return await this.repo.viewReviewTutorAll(); }
+        catch (err) { console.error('Error Services', err.message); }
+    }
+    getReviewBook = async (postID: string) => {
+        try { return await this.repo.viewShareNote(postID); }
+        catch (err) { console.error('Error Services', err.message); }
+    }
+    getReviewBookAll = async () => {
+        try { return await this.repo.viewReviewBookAll(); }
+        catch (err) { console.error('Error Services', err.message); }
+    }
 
     checkOwner = async (postID, userID) => {
-        if(postID == userID) return true;
+        if (postID == userID) return true;
         else return false;
     }
 }
