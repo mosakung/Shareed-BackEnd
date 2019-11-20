fdbf2f3b69fc73e1626ac45e42c208ea49725aad
BearSouL <mosakung@live.com>
2019-11-21 00:51:14 +0700

    Update binding

--- src/binding.ts
+++ src/binding.ts
@@ -6,10 +6,16 @@ import ViewControllers from './feature/View/viewControllers';
 import ViewRepository from './feature/View/viewRepository';
 import ViewServices from './feature/View/viewServices';
 
+import DeleteControllers from './feature/Delete/deleteControllers';
+import DeleteRepository from './feature/Delete/deleteRepository';
+import DeleteServices from './feature/Delete/deleteServices';
+import SqlDelete from './feature/Delete/deleteSqlRaw';
+
 export default class Binding {
     private db:database;
 
     private viewControllers: ViewControllers;
+    private deleteControllers: DeleteControllers;
 
     constructor () {
         this.db = new database();
@@ -17,6 +23,12 @@ export default class Binding {
         const viewRepository: ViewRepository = new ViewRepository(this.db);
         const viewServices: ViewServices = new ViewServices(viewRepository);
         this.viewControllers = new ViewControllers(viewServices);
+
+        const sqlDelete : SqlDelete = new SqlDelete();
+        const deleteRepository: DeleteRepository = new DeleteRepository(this.db, sqlDelete);
+        const deleteServices : DeleteServices = new DeleteServices(deleteRepository);
+        this.deleteControllers = new DeleteControllers(deleteServices);
+
     }
 
     getViewControllers() {
