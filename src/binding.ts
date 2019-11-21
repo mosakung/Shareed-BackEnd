6e9b1ad3266f40c182a7b2ec6d58c4b1782ecba0
BearSouL <mosakung@live.com>
2019-11-21 17:50:24 +0700

    implement as wait database

--- src/binding.ts
+++ src/binding.ts
@@ -11,14 +11,15 @@ import DeleteRepository from './feature/Delete/deleteRepository';
 import DeleteServices from './feature/Delete/deleteServices';
 import SqlDelete from './feature/Delete/deleteSqlRaw';
 
+const databaseOut:database = new database();
+
 export default class Binding {
-    private db:database;
+    private db:database = databaseOut;
 
     private viewControllers: ViewControllers;
     private deleteControllers: DeleteControllers;
 
     constructor () {
-        this.db = new database();
 
         const viewRepository: ViewRepository = new ViewRepository(this.db);
         const viewServices: ViewServices = new ViewServices(viewRepository);
