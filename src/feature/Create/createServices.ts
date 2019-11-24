4ff2eb239cdc2c0bf4525d423e5c637cef3dd749
akarapon.ty <akarapon.ty@hotmail.com>
2019-11-24 22:46:02 +0700

    update edit

--- src/feature/Create/createServices.ts
+++ src/feature/Create/createServices.ts
@@ -1,14 +1,31 @@
 import Repo from './createRepository';
+import Validation from '../../../joi/validation'
 
 export default class createServices{
     private repo : Repo;
-    constructor(repo:Repo){
+    private validation : Validation;
+    constructor(repo:Repo, validation: Validation){
         this.repo = repo;
+        this.validation = validation;
     }
 
-    createShareNote = (data: object)=>{
-        return 'fail to create'
+    createShareNote = async (data: object) =>{
+        try{
+            const result = await this.validation.validate(data,'sharenote');
+            return this.repo.createShareNote(result);
+        }
+        catch(err){ throw new Error ("Error Services"); }
     }
 
-    
+    createShareEvent = async (data: object) => {
+        try{
+            const result = await this.validation.validate(data,'shareevent')
+        }
+        catch(err){}
+    }
+
+    createReviewBook = async (data:object) =>{
+
+    }
+
 }
